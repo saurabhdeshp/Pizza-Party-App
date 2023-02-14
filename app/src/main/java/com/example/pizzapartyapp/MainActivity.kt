542209b8b001086c9379479550b46d606dc0fae1
Saurabh Deshpande <deshpandesaurabh98@gmail.com>
2023-02-13 18:58:25 -0800

    Added comments for MainActivity.kt

--- app/src/main/java/com/example/pizzapartyapp/MainActivity.kt
+++ app/src/main/java/com/example/pizzapartyapp/MainActivity.kt
@@ -9,29 +9,52 @@ import com.example.pizzapartyapp.R.*
 
 const val SLICES_PER_PIZZA = 8
 
+/*
+* This class is the activity which will run for the first time
+* In our case this is the page of our pizza party app
+* */
 class MainActivity : AppCompatActivity() {
+    /**
+     * Total people attending the party
+     */
     private lateinit var numAttendEditText: EditText
+
+    /**
+     * Total pizzas calculated
+     */
     private lateinit var numPizzasTextView: TextView
+
+    /**
+     * The hungry ratio radio button - light, medium and ravenous
+     */
     private lateinit var howHungryRadioGroup: RadioGroup
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(layout.activity_main)
+
         numAttendEditText = findViewById(id.number_of_people_edit_text)
         numPizzasTextView = findViewById(id.total_number_of_pizzas_text_view)
         howHungryRadioGroup = findViewById(id.how_hungry_radio_group)
 
     }
 
+
     fun calculateClick(view: View) {
+        // Get the text of number of people at the party
         val numAttendStr = numAttendEditText.text.toString()
+
+        // convert to int
         val numAttend = numAttendStr.toInt()
+
         val slicesPerPerson = when (howHungryRadioGroup.checkedRadioButtonId) {
             id.light_radio_button -> 2
             id.medium_radio_button -> 3
             else -> 4
         }
+
+        // calculate number of pizzas needed
         val totalPizzas = kotlin.math.ceil(
             numAttend * slicesPerPerson /
                     SLICES_PER_PIZZA.toDouble()
